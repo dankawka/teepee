63845c51bfedddc01761d29ce7cc9d510437c0cb
Daniel Kawka <daniel.kawka@outlook.com>
2021-02-07 05:02:24 +0100

    Improve QR code scanner usage

--- lib/services/qr_code_scanner.dart
+++ lib/services/qr_code_scanner.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class _QRViewExampleState extends State<QRViewExample> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  var isFlashOn = false;
   Barcode result;
   QRViewController controller;
 
@@ -26,38 +26,57 @@ class _QRViewExampleState extends State<QRViewExample> {
   Widget build(BuildContext context) {
     final scanArea = (MediaQuery.of(context).size.width < 400 ||
         MediaQuery.of(context).size.height < 400);
-    return WillPopScope(
-      onWillPop: () async {
-        controller?.dispose();
-        return true;
-      },
-      child: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: QRView(
-                  key: qrKey,
-                  onQRViewCreated: _onQRViewCreated,
-                  formatsAllowed: [BarcodeFormat.qrcode],
-                  overlay: QrScannerOverlayShape(
-                    borderColor: Colors.red,
-                    borderRadius: 10,
-                    borderLength: 30,
-                    borderWidth: 10,
-                  )),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                QRView(
+                    key: qrKey,
+                    onQRViewCreated: _onQRViewCreated,
+                    formatsAllowed: [BarcodeFormat.qrcode],
+                    overlay: QrScannerOverlayShape(
+                      borderColor: Colors.red,
+                      borderRadius: 10,
+                      borderLength: 30,
+                      borderWidth: 10,
+                    )),
+                GestureDetector(
+                  onTap: () async {
+                    await controller.toggleFlash();
+                    setState(() {
+                      isFlashOn = !isFlashOn;
+                    });
+                  },
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 32),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      // color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      isFlashOn ? Icons.flash_on : Icons.flash_off,
+                      color: Colors.white,
+                      size: 32,
+                    ),
+                  ),
+                )
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: (result != null)
-                    ? Text(
-                        'Barcode Type: ${describeEnum(result.format)}   Data: ${result.code}')
-                    : Text('Scan a code'),
-              ),
-            )
-          ],
-        ),
+          ),
+          // Expanded(
+          //   flex: 1,
+          //   child: Center(
+          //     child: (result != null)
+          //         ? Text(
+          //             'Barcode Type: ${describeEnum(result.format)}   Data: ${result.code}')
+          //         : Text('Scan a code'),
+          //   ),
+          // )
+        ],
       ),
     );
   }
